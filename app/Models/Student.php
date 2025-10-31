088a8eca6f0674d012b096e6d3f25d8715290fff
BriceHarper <58884670+BriceHarper@users.noreply.github.com>
2025-10-31 01:47:31 +0100

    feat: Add Student seeder and Student factory

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -2,10 +2,30 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Attributes\UseFactory;
 
+/**
+ * @property int $student_id
+ * @property string $first_name
+ * @property string $last_name
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Database\Factories\StudentFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Student newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Student newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Student query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Student whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Student whereFirstName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Student whereLastName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Student whereStudentId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Student whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Student extends Model
 {
+    use HasFactory;
     protected $fillable = [
         "student_id",
         "first_name",
