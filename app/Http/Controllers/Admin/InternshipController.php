bac9b20e7912b0eaec7ec13e925b9d096c7efcb8
BriceHarper <58884670+BriceHarper@users.noreply.github.com>
2025-11-03 22:01:14 +0100

    refactor: Change validation rules

--- app/Http/Controllers/Admin/InternshipController.php
+++ app/Http/Controllers/Admin/InternshipController.php
@@ -3,41 +3,44 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Illuminate\Validation\Rule;
-use App\Models\Internship;
-use Illuminate\Support\Facades\Route;
+use Inertia\Response;
 
 class InternshipController extends Controller
 {
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create(): Response
     {
+        /** @noinspection PhpParamsInspection */
         return Inertia::render("Admin/Internships/Create");
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
-    {   
-        dd($request->all());
+    public function store(Request $request): RedirectResponse
+    {
+
         $data = $request->validate([
-            "company.siren"=>["required"],
             "internship.startDate"=>["required",Rule::date()->format("d/m/Y")],
             "internship.endDate"=>["required",Rule::date()->format("d/m/Y")],
             "internship.isRemote"=>["required", "boolean:strict"],
             "internship.subject"=>["required", "alpha_num:ascii"],
             "internship.studentTask"=>["required", "alpha_num:ascii"],
-            "internship.student"=>["required", "alpha_dash:ascii"],
             "internship.teacher"=>["required"],
-            "supervisor.name"=>["required", "alpha_dash:ascii"],
+            "student.student_id"=>["required", "alpha_dash:ascii"],
+            "company.siren" => ["required", "regex:/^\d{9}$/"],
+            "supervisor.first_name"=>["required", "alpha_dash:ascii"],
+            "supervisor.last_name"=>["required", "alpha_dash:ascii"],
             "supervisor.mail"=>["required", "email"],
             "supervisor.phone"=>["required", "regex:/^(?:(?:\+|00)33|0)\s*[1-9](?:[\s.-]*\d{2}){4}$/"],
-            
+
         ]);
         // dd($data);
         return redirect()->route("internships");
