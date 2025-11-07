c99e4e0c700901ebf28092ef96de5019db409855
BriceHarper <58884670+BriceHarper@users.noreply.github.com>
2025-11-07 23:23:46 +0100

    feat: Routes grouped by rsources and controllers

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,31 @@
 <?php
 use App\Http\Controllers\Student\InternshipController as StudentInternshipController;
 use App\Http\Controllers\Admin\InternshipController as AdminInternshipController;
+use App\Http\Controllers\Admin\TeacherController as AdminTeacherController;
 use App\Http\Controllers\Admin\StudentController as StudentController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-
-Route::get("/internships", [StudentInternshipController::class, "index"])
-        ->name("internships");
 Route::prefix("admin")->name("admin.")->group(function(){
-        Route::get("/internships/create", [AdminInternshipController::class, "create"])
-        ->name("internships.create");
-        Route::post("/internships", [AdminInternshipController::class, "store"])
-        ->name("internships.store");
-        Route::get("/students", [StudentController::class, "index"])
-        ->name("students");
+        Route::prefix("internships")->name("internships.")->group(function(){
+            Route::controller(AdminInternshipController::class)->group(function(){
+                Route::get("/create", "create")->name("create");
+                Route::post("/", "store")->name("store");
+                Route::get("/", "index")->name("index");
+            });
+        });
+
+        Route::prefix("students")->name("students.")->group(function(){
+           Route::controller(StudentController::class)->group(function(){
+                Route::get("/", "index")->name("index");
+           });
+        });
+
+        Route::prefix("teachers")->name("teachers.")->group(function(){
+            Route::controller(AdminTeacherController::class)->group(function(){
+                Route::get("/", "index")->name("index");
+                Route::get("/create", "create")->name("create");
+            });
+        });
 
 });
